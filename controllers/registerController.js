60c44d8c0eb7decbf1b87e26263c6acd2dba514e
Evelyn Paplauskas <evelyn.paplauskas@outlook.com>
2024-03-08 19:27:08 +1100

    Completed MongoDB Async CRUD Operations

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -7,13 +7,16 @@ const handleNewUser = async (req, res) => {
     return res
       .status(400)
       .json({ message: "Username and password are required." });
+
   // check for duplicate usernames in the db
   const duplicate = await User.findOne({ username: user }).exec();
   if (duplicate) return res.sendStatus(409); //Conflict
+
   try {
     //encrypt the password
     const hashedPwd = await bcrypt.hash(pwd, 10);
-    // create and store the new user
+
+    //create and store the new user
     const result = await User.create({
       username: user,
       password: hashedPwd,
